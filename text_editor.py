bea80f5f728f317f0667471c31a2023c1252da34
xtstc131 <734704896@qq.com>
2016-06-01 16:39:12 +0800

    Change_1

--- text_editor.py
+++ text_editor.py
@@ -1,24 +1,41 @@
+#coding=utf-8 
 import sys,os,tkFileDialog
 from Tkinter import *
 
-
+#by xtstc
 class Editor(object):
 	"""docstring for Editor"""
 	def __init__(self):
 		self.tk = Tk()
+
 		self.createUI()
 		self.tk.mainloop()
 	def createUI(self):
 		self.tk.title("XtStC Text Enditor")
 		menubar = Menu(self.tk)
 		fmenu = Menu (menubar,tearoff = 0)
+		Emenu = Menu(menubar ,tearoff = 0)
+		#File选项
 		fmenu.add_command(label = 'Open',command = self.open)
 		fmenu.add_command(label = 'Save',command = self.save)
 		fmenu.add_command(label = 'Exit',command = self.exit)
 		menubar.add_cascade(label = "File",menu = fmenu)
+		#Edit选项
+		Emenu.add_command(label = 'Format')
+		Emenu.add_command(label = 'Statics')
+		Emenu.add_command(label = 'Find & Replace')
+		Emenu.add_command(label = 'Sort')
+		menubar.add_cascade(label = "Edit",menu = Emenu)
+		
 		self.tk.config(menu = menubar)
-		self.text = Text()
-		self.text.pack()
+		self.text = Text(bg = 'black' , fg = 'white')
+		#self.text.grid(sticky = N+E+S+W)
+		#self.scorollbar = Scrollbar(self.text)
+		#self.scorollbar.pack(side = RIGHT,fill = Y)
+		#self.scorollbar.config(command = self.text.yview)
+		#self.text.config(yscrollcommand=self.scorollbar.set)
+		self.text.pack(expand = YES,fill = BOTH)
+		
 	def save(self):
 		txtContent = self.text.get(1.0,END)
 		self.SaveFile(content = txtContent)
@@ -28,6 +45,7 @@ class Editor(object):
 		if fileContent is not -1:
 			self.text.delete(1.0,END)
 			self.text.insert(1.0,fileContent)
+
 
 	def OpenFile(self,fname = None):
 		if fname is None:
