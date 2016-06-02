2b7b91475a8bc4565f3d9165e8a92224f535aabf
xtstc131 <734704896@qq.com>
2016-06-02 16:20:33 +0800

    Change_3

--- text_editor.py
+++ text_editor.py
@@ -9,22 +9,35 @@ class Editor(object):
 		self.tk = Tk()
 		# 设置窗口初始化大小
 		self.tk.geometry('960x540')
+		#建立主UI界面
 		self.createUI()
+		# 绑定快捷键
 		self.bind_key()
 		# 进入主循环
 		self.tk.mainloop()
+	
 	def createUI(self):
+		#设置程序的标题
 		self.tk.title("XtStC Text Enditor")
+		
+		#主菜单栏
 		menubar = Menu(self.tk)
+
+		#主菜单栏的选项卡
 		fmenu = Menu (menubar,tearoff = 0)
 		Emenu = Menu(menubar ,tearoff = 0)
+		
 		# File选项
+		# fmenu.add_command()函数，lable代表选项卡的名字，accelerator代表提示快捷键(但是要具体绑定键盘),command 代表点击该选项卡调用的函数
 		fmenu.add_command(label = 'Open',accelerator='Command+O',command = self.open)
 		fmenu.add_command(label = 'Save',accelerator = 'Command+S',command = self.save)
 		fmenu.add_command(label = 'Exit',accelerator ='Command+Q',command = self.exit)
 		menubar.add_cascade(label = "File",menu = fmenu)
-		# Edit选
-		Emenu.add_command(label = 'Format',command = self.format)
+		
+		# Edit选项
+		# 同上述fmenu
+		Emenu.add_command(label = 'Select All',accelerator = 'Command+A',command = self.select_all)
+		Emenu.add_command(label = 'Format',accelerator = 'Command+F',command = self.format)
 		Emenu.add_command(label = 'Statics',command = self.Statics)
 		Emenu.add_command(label = 'Find & Replace')
 		Emenu.add_command(label = 'Sort')
@@ -32,10 +45,12 @@ class Editor(object):
 		
 		#加入文本框，并让其与滚动条绑定
 		self.tk.config(menu = menubar)
+		#设置文本框背景颜色是‘black’,字的颜色是'white',光标的颜色是'white'
 		self.text = Text(bg = 'black' , fg = 'white',insertbackground = 'white')
 		#self.text.grid(sticky = N+E+S+W)
+		
+		#设置滚动条让其可以随文本框滚动，也可以控制文本框滚动
 		self.scorollbar = Scrollbar(self.text)
-
 		self.scorollbar.pack(side = RIGHT,fill = Y)
 		self.text["yscrollcommand"] = self.scorollbar.set
 		self.scorollbar.config(command = self.text.yview)
@@ -49,6 +64,8 @@ class Editor(object):
 	def bind_key(self):
 		self.tk.bind('<Command-o>',self.open)
 		self.tk.bind('<Command-s>',self.save)
+		self.tk.bind('<Command-f>',self.format)
+		self.tk.bind('<Command-a>',self.select_all)
 	#File菜单的函数
 	def save(self,event = None):
 		txtContent = self.text.get(1.0,END)
@@ -59,13 +76,20 @@ class Editor(object):
 		if fileContent is not -1:
 			self.text.delete(1.0,END)
 			self.text.insert(1.0,fileContent)
+	
 	#Edit菜单的函数
-	def format(self):
+	def format(self,event = None):
 		FormatContent = self.text.get(1.0,END)
 		result = ' '.join(FormatContent.split())
 		self.text.delete(1.0,END)
 		self.text.insert(1.0,result)
-
+	def select_all(self ,event = None):
+		self.text.tag_add(SEL,'1.0',END+'-1c')
+		#下面这两句我也不知道要什么。。。注释掉，这个函数暂时没发现问题
+		#self.text.mark_set(INSERT,'1.0')
+		#self.text.see(INSERT)
+		
+	#open函数的细节函数
 	def OpenFile(self,fname = None):
 		if fname is None:
 			return -1
@@ -74,6 +98,7 @@ class Editor(object):
 		content = file.read()
 		file.close()
 		return content
+	#save函数的细节函数
 	def SaveFile(self,content = None):
 		if content is None:
 			return -1
